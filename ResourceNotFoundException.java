e59bea00d85b3591ed5bdbe1ec95f62928470e21
KAGITHA VENKATA KALYAN <85292829+venkatakalyankagitha@users.noreply.github.com>
2025-02-26 10:33:50 +0530

    Create ResourceNotFoundException.java

---
+++ ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.Student.Management.controller;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String string) {
+		super(string);
+		
+	}
+
+	
+
+}
