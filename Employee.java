92b3869ae46dbbd0d53cdec0afe736a2636b84bd
KAGITHA VENKATA KALYAN <85292829+venkatakalyankagitha@users.noreply.github.com>
2025-02-26 10:28:50 +0530

    Employee.java

---
+++ Employee.java
@@ -0,0 +1,60 @@
+package com.Student.Management.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="empk1")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	long eid;
+	String fname;
+	String lname;
+	int age;
+	double salary;
+	long phone;
+	
+	public long getEid() {
+		return eid;
+	}
+	public void setEid(long eid) {
+		this.eid = eid;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	
+	
+
+}
